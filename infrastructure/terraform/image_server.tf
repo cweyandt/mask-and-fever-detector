5aa95d73415727c5b0ed616d0d026ab17bcd9835
imander <andrsn84@gmail.com>
2020-11-21 21:14:34 -0800

    Add postgres and metabase

--- infrastructure/terraform/image_server.tf
+++ infrastructure/terraform/image_server.tf
@@ -55,6 +55,14 @@ resource "aws_security_group" "w251_image_server" {
     cidr_blocks = ["${data.external.myipaddr.result.ip}/32"]
   }
 
+  ingress {
+    description = "Metabase from public IP"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["${data.external.myipaddr.result.ip}/32"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
